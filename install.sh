2a46f8764813528f85c53d9d73e0a26a9a95c277
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2019-08-14 16:49:45 +0200

    :sparkles: Install bundler

--- install.sh
+++ install.sh
@@ -160,12 +160,15 @@ nvm install 8.9.4
 sudo apt-get -y install ruby
 sudo apt-get -y install ruby-dev
 
-# Use RVM though to make life easy
+# RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install 2.5.0
 rvm use 2.5.0
+
+# Bundler
+sudo gem install bundler
 
 #
 # Finish installation
