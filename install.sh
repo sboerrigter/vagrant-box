b0bf8d52587fe4404966a8cfc7132a17691903a4
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2019-08-14 16:23:22 +0200

    :fire: Node packages

--- install.sh
+++ install.sh
@@ -154,16 +154,6 @@ wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 8.9.4
 
-# Node Packages
-sudo npm install -g gulp
-sudo npm install -g grunt
-sudo npm install -g bower
-sudo npm install -g yo
-sudo npm install -g browser-sync
-sudo npm install -g browserify
-sudo npm install -g pm2
-sudo npm install -g webpack
-
 #
 # Ruby
 #
