a539041e2f44d5cdd24a3588f4bdb60f5a8cc446
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2019-08-15 11:29:52 +0200

    :bug: Fix `    default: dpkg-preconfigure: unable to re-open stdin: No such file or directory` error

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # Package manager
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 sudo apt-get install -y build-essential
