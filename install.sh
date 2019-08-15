e480a71592ab7f552f4fa79fa0e34088a2837361
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2019-08-15 09:16:44 +0200

    :wrench: Don't create scotchbox database

--- install.sh
+++ install.sh
@@ -113,7 +113,6 @@ sudo service apache2 restart
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 sudo apt-get -y install mysql-server
-sudo mysqladmin -uroot -proot create scotchbox
 sudo apt-get -y install php7.3-mysql
 sudo service apache2 restart
 
